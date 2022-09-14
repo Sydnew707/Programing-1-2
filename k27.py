46fcc4930c85197a04c25139f5707768aaedda92
Sydnew707 <112713191+Sydnew707@users.noreply.github.com>
2022-09-14 14:11:40 +0000

    update

--- k27.py
+++ k27.py
@@ -3,7 +3,34 @@ from stanfordkarel import *
 
 def main():
     """ Karel code goes here! """
-    
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    pick_beeper()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    move()
     pass
 
 
